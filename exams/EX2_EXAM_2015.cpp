7a8b97a0d6206a13cdadac08a13337572d2cc4f0
Maryem Bamou <34491538+int-j@users.noreply.github.com>
2021-09-06 18:35:22 +0100

    change path

--- exams/EX2_EXAM_2015.cpp
+++ exams/EX2_EXAM_2015.cpp
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 main()
 {
- //1-Déclaration et allocation dynamique de la mémoire 1 point
+ //1-DÃ©claration et allocation dynamique de la mÃ©moire 1 point
  int N, *T, *P1,i,j,c=0,t=0;
 
  printf(" Entrez N \n ");
@@ -23,7 +23,7 @@ for (P1=T;P1<T+N;P1++)
      printf("%d \t", *P1);
   printf("\n");
   
-//2-Déterminer si la coupe est équilibrée 2.5 points
+//2-DÃ©terminer si la coupe est Ã©quilibrÃ©e 2.5 points
 // 0.25 point pour la saisie des i et j
 printf("Entrez la valeur de i");
 scanf("%d",&i);
@@ -37,8 +37,8 @@ for(int *P=T+i; P<=T+j; P++){
 if(c==t) printf("la partie est equilibree");
 
 else printf("la partie n'est pas equilibree");
-// 3-chercher dans le tableau le nombre de plus petites coupes équilibrées 2.5 points (c a d une fois je trouve que nbre de 1 = nbre de 0 j'incrémente le compteur de coupes 
-//et je réinitialise les compteurs de 0 et de 1 à O )
+// 3-chercher dans le tableau le nombre de plus petites coupes Ã©quilibrÃ©es 2.5 points (c a d une fois je trouve que nbre de 1 = nbre de 0 j'incrÃ©mente le compteur de coupes 
+//et je rÃ©initialise les compteurs de 0 et de 1 Ã  O )
 int z=0;
 c=0,t=0; 
 int *P;
@@ -49,7 +49,7 @@ for(P=T;P<=T+N-1;P++){
 	          c=0;
 			  t=0;}
 }
-printf("le nombre de parties équilibrées trouvées %d",z);
+printf("le nombre de parties Ã©quilibrÃ©es trouvÃ©es %d",z);
 printf("\n");
 system("pause");
 }	
